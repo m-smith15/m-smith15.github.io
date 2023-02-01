39cb903c121d5e7660d38b77bd73f1a2e115ab85
Mack Smith <106689709+m-smith15@users.noreply.github.com>
2023-02-01 04:18:59 -0500

    adding in title card pausing to scripting

--- scripting.js
+++ scripting.js
@@ -1,11 +1,17 @@
 const pauseAnimations = () => {
     let imageAnimation = [];
+    let titleAnimation = [];
     for(var x = 1; x  < 4; x++){
-        let temp = document.querySelector('.project-slideshow li:nth-child('+ x +') span');
-        imageAnimation.push(temp);
+        let tempImage = document.querySelector('.project-slideshow li:nth-child('+ x +') span');
+        imageAnimation.push(tempImage);
+        let tempTitle = document.querySelector('.project-slideshow li:nth-child('+ x+ ') div');
+        imageAnimation.push(tempTitle);
     }
     imageAnimation.forEach(image => {
         image.style.animationPlayState = 'paused';
+    })
+    titleAnimation.forEach(title => {
+        title.style.animationPlayState = 'paused';
     })
 
 }
